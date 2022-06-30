99d3d67908bd9983e7ff54e8b7b96e02743dab9b
waLLxAck <svilen.petrov97@gmail.com>
2022-06-30 08:03:09 +0100

    Added URLs section allowing for project related third-party bookmarks

--- Project.py
+++ Project.py
@@ -22,6 +22,10 @@ class StandardFileNames:
     def get_swagger_environment_name(self, project_short_name, project_full_name, environment):
         return f"{project_short_name} - {project_full_name} - {self.swagger_base} - {environment.upper()} - Unit4"
 
+    @staticmethod
+    def get_url_name(project_short_name, project_full_name, url_name):
+        return f"{project_short_name} - {project_full_name} - {url_name} - Unit4"
+
 
 class Auth:
     def __init__(self, company_id, access_token_url, client_id, client_secret, **kwargs):
@@ -51,13 +55,26 @@ class Environment:
         return cls(**json_object)
 
 
+class ProjectUrl:
+    def __init__(self, name, url, **kwargs):
+        self.name: str = name
+        self.url: str = url
+
+    @classmethod
+    def from_json(cls, json_object):
+        return cls(**json_object)
+
+
 class Project:
-    def __init__(self, project_name, swagger_api: str, environments, **kwargs):
+    def __init__(self, project_name, swagger_api: str, urls, environments, **kwargs):
         self.__environments = []
         for environment in environments:
             self.__environments.append(Environment.from_json(environment))
         self.__project_name = project_name
         self.__swagger_api = swagger_api
+        self.__urls = []
+        for url in urls:
+            self.__urls.append(ProjectUrl.from_json(url))
         self.bm = BookmarkManager()
         self.names = StandardFileNames()
         self.__project_short_name = self.get_project_short_name()
@@ -72,16 +89,18 @@ class Project:
             if not environment.erp_lab:
                 if not environment.extension_kit:
                     continue
-                return environment.extension_kit.split("/")[-1].split("-")[1].upper()
+                try:
+                    return environment.extension_kit.split("/")[-1].split("-")[1].upper()
+                except:
+                    return environment.extension_kit.split("/")[-1].split("_")[1].upper()
             return environment.erp_lab.split("/")[-1].split("_")[1].upper()
         raise Exception("Project shortname unknown. Extension Kit url splits on '-' and ERP Lab splits on '_'.")
 
-    def __create_swagger_bookmark(self, project_folder):
-        swagger_name = self.names.get_swagger_name(project_short_name=self.__project_short_name,
-                                                   project_full_name=self.__project_name)
-        self.bm.create_url(project_folder, swagger_name, self.__swagger_api)
-
-    def __create_environment_bookmarks(self, project_folder):
+    def __create_project_bookmarks(self, project_folder):
+        if self.__swagger_api:
+            swagger_name = self.names.get_swagger_name(project_short_name=self.__project_short_name,
+                                                       project_full_name=self.__project_name)
+            self.bm.create_url(project_folder, swagger_name, self.__swagger_api)
         for environment in self.__environments:
             if environment.extension_kit:
                 bookmark_name = self.names.get_extension_kit_name(project_short_name=self.__project_short_name,
@@ -97,14 +116,18 @@ class Project:
                 bookmark_name = self.names.get_swagger_environment_name(project_short_name=self.__project_short_name,
                                                                         project_full_name=self.__project_name,
                                                                         environment=environment.name)
-                self.bm.create_url(project_folder, bookmark_name, environment.erp_lab)
+                self.bm.create_url(project_folder, bookmark_name, environment.swagger_api)
+        for url in self.__urls:
+            if url.name and url.url:
+                bookmark_name = self.names.get_url_name(project_short_name=self.__project_short_name,
+                                                        project_full_name=self.__project_name,
+                                                        url_name=url.name)
+                self.bm.create_url(project_folder, bookmark_name, url.url)
 
     def generate_bookmarks(self):
         unit4_projects_folder = self.bm.get_projects_folder()
         project_folder = self.bm.create_folder(unit4_projects_folder, self.__project_name)
-        if self.__swagger_api:
-            self.__create_swagger_bookmark(project_folder)
-        self.__create_environment_bookmarks(project_folder)
+        self.__create_project_bookmarks(project_folder)
         self.bm.commit_changes()
 
     def build_insomnia_collection(self, project_name):
