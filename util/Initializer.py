cbea8a4cd8260e1902e1700f33e6657103cec33d
Svilen Petrov <svilen.petrov97@gmail.com>
2022-07-16 11:52:30 +0100

    Added SETTINGS initialization

--- util/Initializer.py
+++ util/Initializer.py
@@ -2,9 +2,12 @@ import os
 import platform
 from time import time
 
+from model.Settings import Settings
 from util.FileHandler import FileHandler
 
 FILE_HANDLER = FileHandler()
+SETTINGS = Settings.from_json(FILE_HANDLER.get_settings())
+
 time_now = int(time())
 if platform.system() == "Windows":
     app_data_path = "\\".join(os.getenv('APPDATA').split("\\")[:-1])
