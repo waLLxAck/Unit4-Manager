b1a10aab43d5ead1d1659c2ee9ff750d39ee9006
Svilen Petrov <svilen.petrov97@gmail.com>
2022-07-28 11:16:27 +0100

    Added delimiter option

--- development_tool/csv_header_into_liquid_variables.py
+++ development_tool/csv_header_into_liquid_variables.py
@@ -1,13 +1,13 @@
 import re
 
 
-def convert_header(string_header, string_row_variable):
-    result = "{" + f"%- assign header_item = {string_row_variable} | string.split ';' -%" + "}\n\n"
+def convert_header(string_header, string_row_variable, delimiter):
+    result = "{" + f"%- assign header_item = {string_row_variable} | string.strip_newlines | string.strip  | string.split '{delimiter}' -%" + "}\n\n"
 
-    for i, field in enumerate(string_header.split(";")):
+    for i, field in enumerate(string_header.split(delimiter)):
         field = field.lower()
         field = field.strip().replace(" ", "_").replace("(", "").replace(")", "").replace(".", "").replace("-", "_")
         field = re.sub("[0-9]_", "", field)
         result += "{" + f"%- assign {field} = header_item[{i}] -%" + "}\n"
 
-    print(result)   # todo display in interface
+    print(result)  # todo display in interface
