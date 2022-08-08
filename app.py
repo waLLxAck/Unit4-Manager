0e6d720f98916ddc95f07b67a3d14d05920fa62c
Svilen Petrov <svilen.petrov97@gmail.com>
2022-08-08 13:16:37 +0100

    Renamed; removed main method

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from util.Initializer import FILE_HANDLER
 
 
 def run():
+    FILE_HANDLER.create_folders_if_not_exists()
+    FILE_HANDLER.create_settings_file_if_not_exists()
     settings = Settings.from_json(FILE_HANDLER.get_settings())
     project_files = FILE_HANDLER.get_projects_files()
     for project_file in project_files:
@@ -12,7 +14,3 @@ def run():
     print("Bookmarks file updated.")
     if settings.distribute_bookmarks_between_browsers:
         FILE_HANDLER.distribute_bookmarks_from_default_to_other_browsers()
-
-
-if __name__ == '__main__':
-    run()
